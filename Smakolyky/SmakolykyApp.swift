34f589ff03f8daf1a134b3d709fe7b700df49d98
Alex Vasilyev <olorin2007@gmail.com>
2022-08-16 20:07:18 +0200

    Initial Commit

---
+++ Smakolyky/SmakolykyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmakolykyApp.swift
+//  Smakolyky
+//
+//  Created by Alex Vasyliev on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SmakolykyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

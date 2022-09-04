e8e8437a52a11d59411d2533dd6e7e358e4746e1
Alex Vasilyev <olorin2007@gmail.com>
2022-09-04 21:54:49 +0200

    Prepare UI for Account screen

--- Smakolyky/Screens/AccountView/AccountView.swift
+++ Smakolyky/Screens/AccountView/AccountView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthday = Date()
+    @State private var extraNapkins = false
+    @State private var extraCheese = false
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+
+                } header: {
+                    Text("Personal Details")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Extra cheese", isOn: $extraCheese)
+                        
+                } header: {
+                    Text("Requests")
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+
+            }
+            .navigationTitle("Account")
         }
     }
 }

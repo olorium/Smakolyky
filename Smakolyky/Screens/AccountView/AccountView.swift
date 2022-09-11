203a28d5e6d0f54105a41ee52d9702beb30775d3
Alex Vasilyev <olorin2007@gmail.com>
2022-09-11 22:06:48 +0200

    Update form UX

--- Smakolyky/Screens/AccountView/AccountView.swift
+++ Smakolyky/Screens/AccountView/AccountView.swift
@@ -8,47 +8,109 @@
 import SwiftUI
 
 struct AccountView: View {
+  
+    ///Enum for tracking focus on text fields
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     @StateObject var viewModel = AccountViewModel()
+    @available(iOS 15.0, *) @FocusState private var focusTextField: FormTextField?
     
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    TextField("First Name", text: $viewModel.user.firstName)
-                    TextField("Last Name", text: $viewModel.user.lastName)
-                    TextField("Email", text: $viewModel.user.email)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                        .disableAutocorrection(true)
-                    DatePicker("Birthday", selection: $viewModel.user.birthday, displayedComponents: .date)
-                    Button {
-                        viewModel.saveChanges()
-                    } label: {
-                        Text("Save Changes")
-                    }
-
-                } header: {
-                    Text("Personal Details")
-                }
-                
-                Section {
-                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
-                    Toggle("Extra cheese", isOn: $viewModel.user.extraCheese)
+        if #available(iOS 15.0, *) {
+            NavigationView {
+                Form {
+                    Section {
+                        TextField("First Name", text: $viewModel.user.firstName)
+                            .focused($focusTextField, equals: .firstName)
+                            .onSubmit { focusTextField = .lastName }
+                            .submitLabel(.next)
+                        TextField("Last Name", text: $viewModel.user.lastName)
+                            .focused($focusTextField, equals: .lastName)
+                            .onSubmit { focusTextField = .email }
+                            .submitLabel(.next)
+                        TextField("Email", text: $viewModel.user.email)
+                            .focused($focusTextField, equals: .email)
+                            .onSubmit { focusTextField = nil }
+                            .submitLabel(.next)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                        DatePicker("Birthday", selection: $viewModel.user.birthday, displayedComponents: .date)
+                        Button {
+                            viewModel.saveChanges()
+                        } label: {
+                            Text("Save Changes")
+                        }
                         
-                } header: {
-                    Text("Requests")
+                    } header: {
+                        Text("Personal Details")
+                    }
+                    
+                    Section {
+                        Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                        Toggle("Extra cheese", isOn: $viewModel.user.extraCheese)
+                        
+                    } header: {
+                        Text("Requests")
+                    }
+                    .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+                    
                 }
-                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
-
+                .navigationTitle("Account")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button("Dismiss") { focusTextField = nil }
+                        .tint(.red)
+                    }
+                }
             }
-            .navigationTitle("Account")
-        }
-        .onAppear {
-            viewModel.retrieveUser()
-        }
-        .alert(item: $viewModel.alertItem) { alertItem in
-            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            .onAppear {
+                viewModel.retrieveUser()
+            }
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            }
+        } else {
+            NavigationView {
+                Form {
+                    Section {
+                        TextField("First Name", text: $viewModel.user.firstName)
+                        TextField("Last Name", text: $viewModel.user.lastName)
+                        TextField("Email", text: $viewModel.user.email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                        DatePicker("Birthday", selection: $viewModel.user.birthday, displayedComponents: .date)
+                        Button {
+                            viewModel.saveChanges()
+                        } label: {
+                            Text("Save Changes")
+                        }
+                        
+                    } header: {
+                        Text("Personal Details")
+                    }
+                    
+                    Section {
+                        Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                        Toggle("Extra cheese", isOn: $viewModel.user.extraCheese)
+                        
+                    } header: {
+                        Text("Requests")
+                    }
+                    .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+                    
+                }
+                .navigationTitle("Account")
+            }
+            .onAppear {
+                viewModel.retrieveUser()
+            }
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
